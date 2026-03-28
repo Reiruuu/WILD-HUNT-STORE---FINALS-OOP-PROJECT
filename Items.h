43f8af70d22f97493b1e11c6ab093faf6d094e09
Rei.ru <hiyoritsukiyuki@gmail.com>
2026-03-28 12:48:46 +0800

    Add files via upload

---
+++ Items.h
@@ -0,0 +1,42 @@
+// COMPROG FINAL PROJECT =======================================================================================
+// PROJECT WILD HUNT STORE
+
+
+#ifndef ITEMS_H_INCLUDED
+#define ITEMS_H_INCLUDED
+using namespace std;
+
+
+// DERIVED CLASS ITEMS ==========================================================================================
+class Item : public ObjEntity {
+private://ENCAPSULATION__________________________________________________________________________________________
+    string Type;
+
+public:
+    //DEFAULT CONSTRUCTOR________________________________________________________________________________________
+    Item() :
+        ObjEntity(),
+        Type("") {}
+    //CONSTRUCTOR________________________________________________________________________________________________
+    Item(string OBN, int AMT, bool AVAIL, string TP)
+        : ObjEntity(OBN, AMT, AVAIL), Type(TP) {}
+
+
+    //POLYMORPHISM_______________________________________________________________________________________________
+    void ShowEntity() const override {
+        cout << "| Name: " << GetOBN() << endl
+             << "| Type: " << Type << endl
+             << "| Stock: " << GetQTY() << endl;
+    }
+
+    // DATA SAVING TO FILE FOR ITEMS_____________________________________________________________________________
+    void SaveEntity(ofstream& out) const override {
+        out << "ITEM" << endl;
+        out << GetOBN() << endl;
+        out << Type << endl;
+        out << GetQTY() << endl;
+    }
+};
+#endif // ITEMS_H_INCLUDED
+// ==============================================================================================================
+// KYM ANDREI LEGASPI / BSCS 12A1
---
+++ Weapons.h
@@ -0,0 +1,47 @@
+// COMPROG FINAL PROJECT =======================================================================================
+// PROJECT WILD HUNT STORE
+
+#ifndef WEAPONS_H_INCLUDED
+#define WEAPONS_H_INCLUDED
+using namespace std;
+
+
+
+// DERIVED CLASS WEAPONS =======================================================================================
+class Weapon : public ObjEntity {
+private://ENCAPSULATION_________________________________________________________________________________________
+    string WPType;
+    int Damage;
+
+public:
+    //DEFAULT CONSTRUCTOR_______________________________________________________________________________________
+    Weapon() :
+        ObjEntity(),
+        WPType(""),
+        Damage(0) {}
+
+    //CONSTRUCTOR_______________________________________________________________________________________________
+    Weapon(string OBN, int AMT, bool AVAIL, string WTP, int DMG)
+        : ObjEntity(OBN, AMT, AVAIL), WPType(WTP), Damage(DMG) {}
+
+
+    //POLYMORPHISM______________________________________________________________________________________________
+    void ShowEntity() const override {
+        cout << "| Name: " << GetOBN() << endl
+             << "| Type: " << WPType << endl
+             << "| Damage Stat: " << Damage << endl
+             << "| Stock: " << GetQTY() << endl;
+    }
+
+    //DATA SAVING TO FILE FOR WEAPONS__________________________________________________________________________
+    void SaveEntity(ofstream& out) const override {
+        out << "WEAPON" << endl;
+        out << GetOBN() << endl;
+        out << WPType << endl;
+        out << Damage << endl;
+        out << GetQTY() << endl;
+    }
+};
+#endif // WEAPONS_H_INCLUDED
+// ==============================================================================================================
+// KYM ANDREI LEGASPI / BSCS 12A1
